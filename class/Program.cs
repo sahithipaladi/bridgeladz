bc5e75cae667342806131b95665dad5df2491d6e
sahithi <sahithipari99@gmail.com>
2021-06-25 18:41:34 +0530

    First Program

---
+++ class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace oops
+{
+    class Program
+    {
+
+        public void MainFunction()
+        {
+            Console.WriteLine("Main class");
+        }
+        static void Main(string[] args)
+        {
+            //main class instance
+            Program obj = new Program();
+            obj.MainFunction();
+
+            //other class instace
+            demo dObj = new demo();
+            dObj.addition();
+        }
+    }
+
+    class demo
+    {
+        int x = 10;
+        int y = 20;
+        int z;
+
+        public void addition()
+        {
+            z = x + y;
+            Console.WriteLine("other class in Namespace");
+            Console.WriteLine(z);
+        }
+    }
+}s
